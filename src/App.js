9a6395e5d91fc19c8893726533f403a29a894fa9
Liam <112492930+ElSammen@users.noreply.github.com>
2023-04-25 22:42:57 +0100

    initial file dump

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.scss';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./Components/Home/home";
+import Projects from './Components/Projects/Projects';
+import CV from './Components/CV/CV';
+import Navbar from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <Router>
+        <Navbar /> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/CV" element={<CV />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/home.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Typewriter from 'typewriter-effect';
+
+
+function home() {
+  return (
+    <Container className='homePage'>
+      <div className="intro">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString('Liam Salmon')
+              .callFunction(state => {
+                state.elements.cursor.setAttribute('hidden', 'hidden');
+                typewriter.stop();
+              })
+              .start();
+          }}
+        />
+        <h1>Full Stack Web Developer</h1>
+
+      </div>
+      <div className="homeBody d-flex">
+        <div className="homeBody1">
+          <div className="homeBody1TextBox">
+            <p className="homeBody1Text">A recent graduate from <a className="devAcadLink" href="https://thedeveloperacademy.com/"><em>The Developer Academy</em></a>'s Full Stack Web Developer bootcamp, I've a keen interest in the production of clean, functional websites, with crisp easily maintained code.</p>
+            <p className="homeBody1Text"></p>
+            <p className="homeBody1Text">Check me out below or get in touch and lets see how we can help eachother.</p>
+          </div>
+          <div className="contactBox"></div>
+          <a target="_blank" href="https://www.linkedin.com/in/liam-salmon-9053b8265/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkedin profile" className="homeBodyImg pageLink" width="35px"></img></a>
+          <a target="_blank" href="https://github.com/ElSammen"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github link" className="homeBodyImg pageLink" width="35px"></img></a>
+          <a target="_blank" href="mailto://liam_salmon@live.co.uk" ><img src="https://cdn-icons-png.flaticon.com/512/546/546394.png" alt="compose new mail link" className="homeBodyImg pageLink" width="35px"></img></a>
+        </div>
+        <div className="homeBody2">
+          <p className="homeBody2Text">Skillset:</p>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" alt="JavaScript" className="homeBodyImg JSLogo" width="35px" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png" alt="Git" className="homeBodyImg" width="35px" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" alt="React" className="homeBodyImg" width="35px" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS" className="homeBodyImg" width="35px" />
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export default home
